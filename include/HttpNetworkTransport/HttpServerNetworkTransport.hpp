34d83e493a4cdbd431b3a551049759d284603834
Hatem Nabli <hatem.nabli@actemium.com>
2024-07-03 01:50:54 +0200

    feat: implementation of the HttpServerNetworkTransport to use the network available through the operating system

--- include/HttpNetworkTransport/HttpServerNetworkTransport.hpp
+++ include/HttpNetworkTransport/HttpServerNetworkTransport.hpp
@@ -1,5 +1,5 @@
-#ifndef HTTP_SERVER_NETWORK_TRANSPORT_HPP
-#define HTTP_SERVER_NETWORK_TRANSPORT_HPP
+#ifndef HTTP_NETWORK_TRANSPORT_HTTP_SERVER_NETWORK_TRANSPORT_HPP
+#define HTTP_NETWORK_TRANSPORT_HTTP_SERVER_NETWORK_TRANSPORT_HPP
 
 /**
  * @file HttpServerNetworkTransport.hpp
@@ -9,9 +9,59 @@
  * 
  * © 2024 by Hatem Nabli
  */
-namespace HttpServerNetworkTransport {
+
+#include <memory>
+#include <Http/ServerTransportLayer.hpp>
+namespace HttpNetworkTransport {
+
+    /**
+     * This is an implementation of Http::HttpServerNetworkTransport
+     * that uses the real network available through the operating system.
+     */
+    class HttpServerNetworkTransport : public Http::ServerTransportLayer
+    {
+        // LifeCycle managment
+    public:
+        ~HttpServerNetworkTransport();
+        HttpServerNetworkTransport(const HttpServerNetworkTransport&) = delete; // Copy Constructor that creates a new object by making a copy of an existing object. 
+        //It ensures that a deep copy is performed if the object contains dynamically allocated resources 
+        HttpServerNetworkTransport(HttpServerNetworkTransport&&); // Move Constructor that transfers resources from an expiring object to a newly constructed object.
+        HttpServerNetworkTransport& operator=(const HttpServerNetworkTransport&) = delete; //Copy Assignment Operation That assigns the values of one object to another object using the assignment operator (=)
+        HttpServerNetworkTransport& operator=(HttpServerNetworkTransport&&); //Move Assignment Operator: Amove assignment operator efficiently transfers resources from one object to another.
+
+    public:
+        /**
+        * This is the default constructor
+        */
+        HttpServerNetworkTransport();
+
+    public:
+        virtual bool BindNetwork(
+            uint16_t port,
+            NewConnectionDelegate newConnectionDelegate
+        ) override;
+        virtual uint16_t GetBoundPort() override;
+        virtual void ReleaseNetwork() override;
+
+        // Private properties
+    private:
+        /* data */
+
+        /**
+         * This is the type of structure that contains the private
+         * properties of the instance. It is defined in the implementation
+         * and declared here to ensure that iwt is scoped inside the class.
+        */
+        struct Impl;
+
+        /**
+        * This contains the private properties of the instance.
+        */       
+        std::unique_ptr<struct Impl> impl_;
+    };
+    
 
 }
 
 
-#endif /* HTTP_SERVER_NETWORK_TRANSPORT_HPP */
+#endif /* HTTP_NETWORK_TRANSPORT_HTTP_SERVER_NETWORK_TRANSPORT_HPP */
--- src/HttpServerNetworkTransport.cpp
+++ src/HttpServerNetworkTransport.cpp
@@ -7,3 +7,148 @@
  * © 2024 by Hatem Nabli
  */
 
+#include <SystemUtils/NetworkEndPoint.hpp>
+#include <SystemUtils/NetworkConnection.hpp>
+#include <HttpNetworkTransport/HttpServerNetworkTransport.hpp>
+#include <Http/Connection.hpp>
+
+namespace {
+    /**
+     * This calss is an adapter between two related classes in different
+     * libraries:
+     * Http::Connection -- The interface required by the Http library
+     *      for sending and receiving data across the transport layer.
+     * SystemUtils::NetworkConnection -- The class which implements
+     *      a connection object in terms of the operating system's network APIs.
+     */
+    struct ConnectionAdapter
+        : public Http::Connection
+    {
+        /* Properties */
+
+        /**
+         * This is the object which implementing the network connection
+         * in terms of operating system's network APIs.
+         */
+        std::shared_ptr< SystemUtils::NetworkConnection > adapter;
+        
+        /**
+         * This is the delegate to call whenever data is received
+         * from the remote peer.
+         */
+        DataReceivedDelegate dataReceivedDelegate;
+
+        /**
+         * This is the delegate to call to notify the user that the connection
+         * has been broken.   
+         */
+        BrokenDelegate brokenDelegate;
+
+        /* Methods */
+
+        /**
+         * This metho dshould be called once the adapter is in place.
+         * It fires up the actual network processing.
+         * 
+         * @return
+         *      An indication of whether or not the method was
+         *      successful is returned.
+         */
+        bool WireUpAdapter() {
+            return adapter->Process(
+                [this](const std::vector< uint8_t >& message){
+                    if (dataReceivedDelegate != nullptr) {
+                        dataReceivedDelegate(message);
+                    }
+                },
+                [this](bool graceful){
+                    if (brokenDelegate != nullptr) {
+                        brokenDelegate(graceful);
+                    }
+                }
+            );
+        }
+
+        /* Http::Connection */
+        virtual void SetDataReceivedDelegate(DataReceivedDelegate newDataReceivedDelegate) override {
+            dataReceivedDelegate = newDataReceivedDelegate;
+        }
+
+        virtual void SetConnectionBrokenDelegate(BrokenDelegate newBrokenDelegate) override {
+            brokenDelegate = newBrokenDelegate;
+        }
+
+        virtual void SendData(std::vector< uint8_t > data) override {
+            adapter->SendMessage(data);
+        }
+
+        virtual void Break(bool clean) override {
+            // TODO: If clean == true, we may need to hold off until
+            // adapter has written out all its data
+            adapter->Close(clean);
+        }
+    };
+    
+}
+
+namespace HttpNetworkTransport {
+
+    /**
+     * This caontains the private properties of a HttpServerNetworkTransport instance.
+     */
+    struct HttpServerNetworkTransport::Impl
+    {
+        /* data */
+        /**
+         * 
+         */
+        SystemUtils::NetworkEndPoint endpoint;
+    };
+
+    HttpServerNetworkTransport::~HttpServerNetworkTransport() = default;
+    HttpServerNetworkTransport::HttpServerNetworkTransport(): impl_(new Impl) 
+    {
+
+    }
+
+    bool HttpServerNetworkTransport::BindNetwork(
+        uint16_t port,
+        NewConnectionDelegate newConnectionDelegate
+    ) {
+        return impl_->endpoint.Open(
+            [
+                this,
+                newConnectionDelegate
+            ](std::shared_ptr< SystemUtils::NetworkConnection > newConnection){
+                const auto adapter = std::make_shared< ConnectionAdapter >();
+                adapter->adapter = newConnection;
+                if (!adapter->WireUpAdapter()) {
+                    return;
+                }
+                newConnectionDelegate(adapter);
+            },
+            [this](
+                uint32_t address,
+                uint16_t port,
+                const std::vector< uint8_t >& body
+            ){
+                // Note: This should never be called, because it's only used
+                // for datagram-oriented network endpoint, and we're 
+                // explicitily configuring this one as a connection-oriented
+                // endpoint.
+            },
+            SystemUtils::NetworkEndPoint::Mode::Connection,
+            0,
+            0,
+            port
+        ); 
+    }
+    
+    uint16_t HttpServerNetworkTransport::GetBoundPort() {
+        return impl_->endpoint.GetBoundPort();
+    }
+
+    void HttpServerNetworkTransport::ReleaseNetwork() {
+        return impl_->endpoint.Close();
+    }
+}
